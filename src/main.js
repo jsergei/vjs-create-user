4ccd92ae5f20325d384facd30d0aa8d840c78d85
Sergei Cherednichenko <sergeych92@gmail.com>
2019-10-02 17:03:12 +0200

    add username validation html

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
-import '../styles/styles.scss';
+import '../styles/main.scss';
 import '../icons/question_mark.png';
+import '../icons/validation-tick-valid.png';
+import '../icons/validation-tick-invalid.png';
