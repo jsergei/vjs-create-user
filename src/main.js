272654aeac24307a37f97128e908d9738f1b821f
Sergei Cherednichenko <sergeych92@gmail.com>
2019-10-03 18:33:24 +0200

    implement custom validation rule for username

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+// css
 import '../styles/main.scss';
+
+// icons
 import '../icons/question_mark.png';
 import '../icons/validation-tick-valid.png';
 import '../icons/validation-tick-invalid.png';
+
+import { UsernameValidator } from './validation';
+
+const usernameEl = document.getElementById('username');
+const usernameValContent = document.querySelector('.validation-content__username');
+let usernameValidator = new UsernameValidator(usernameEl, usernameValContent);
+
+usernameValidator.init();
