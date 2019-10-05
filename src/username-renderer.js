216e4850e2c52d1e3b48f060152a58635e3c52b3
Sergei Cherednichenko <sergeych92@gmail.com>
2019-10-05 19:53:39 +0200

    bugfixing

--- src/username-renderer.js
+++ src/username-renderer.js
@@ -17,9 +17,9 @@ export class UsernameRenderer extends ControlRenderer {
             'Start with a letter or an underscore');
         this._following3SymbolsNode = this._createValRuleNode(state.following3Symbols,
             'Follow with 3 letters');
-        this._remainingAnyNode = this._createValRuleNode(state.remainingAnyNode,
+        this._remainingAnyNode = this._createValRuleNode(state.remainingAny,
             'Then contain any number of letters, digits, underscores, or dots');
-        this._lengthRangeNode = this._createValRuleNode(state.lengthRangeNode,
+        this._lengthRangeNode = this._createValRuleNode(state.lengthRange,
             'Between 4 and 20 symbols long');
 
         const liFragment = new DocumentFragment();
@@ -36,7 +36,7 @@ export class UsernameRenderer extends ControlRenderer {
     _updateContent(state) {
         this._updateValRuleNodeState(this._firstSymbolNode, state.firstSymbol);
         this._updateValRuleNodeState(this._following3SymbolsNode, state.following3Symbols);
-        this._updateValRuleNodeState(this._remainingAnyNode, state.remainingAnyNode);
-        this._updateValRuleNodeState(this._lengthRangeNode, state.lengthRangeNode);
+        this._updateValRuleNodeState(this._remainingAnyNode, state.remainingAny);
+        this._updateValRuleNodeState(this._lengthRangeNode, state.lengthRange);
     }
 }
